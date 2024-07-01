62f037429abb1ae7d322a4f81e709b404886a95c
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2024-07-01 17:46:59 -0500

    update to new version of strapi

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { LogoutButton } from "@/components/Logout";
 import React from "react";
 
 export default function Dashboard() {
-  return <div className="container mx-auto flex justify-between items-center my-4">
+  return <div className="container mx-auto px-4 flex justify-between items-center my-4">
     <h2 className="text-xl">Dashboard</h2>
     <LogoutButton />
   </div>;
